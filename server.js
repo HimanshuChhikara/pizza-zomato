ae671a8fa5ebcff44b1639fe0a9d51b84e54b473
Himanshu <himanshu.chhikara@oimedia.in>
2020-11-26 22:11:03 +0530

    login register

--- server.js
+++ server.js
@@ -19,9 +19,16 @@ app.get('/', function(req,res){
 })
 
 app.get('/cart',function(req,res){
-    res.render('customers/cart')
+    res.render('/views/customers/cart')
 })
 
+app.get('/login',function(req,res){
+    res.render('auth/login')
+})
+
+app.get('/register',function(res,res){
+    res.render('auth/register')
+})
 app.listen(PORT,()=>{
     console.log(`Listening on port ${PORT}`);
 })
