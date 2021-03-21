30f8b4c40e2fe4c3257da5f3d14dd2010a6c70f7
Himanshu <himanshu.chhikara@oimedia.in>
2021-03-21 22:23:41 +0530

    pizza data fetch solved from db

--- app/http/controllers/homeController.js
+++ app/http/controllers/homeController.js
@@ -1,13 +1,20 @@
-const Menu = require('../../models/Menu')
+const Menu = require('../../models/menu')
 
 function homeController(){
     return {
 
-        async index(req,res){
-            const pizzas = await Menu.find()    
-            console.log(pizzas)            
-            return res.render('home', {pizzas,pizzas})
-        } 
+        index(req,res){
+            Menu.find().then(function(pizzas){
+                console.log(pizzas)
+                return res.render('home',{pizzas:pizzas})
+            })
+        }
+
+        // async index(req,res){
+        //     const pizzas = await Menu.find()    
+        //     console.log(pizzas)            
+        //     return res.render('home', {pizzas,pizzas})
+        // } 
     }
 }
 
