c1d83775f561eaff04fb26f402ff02025e48cf9e
Himanshu <himanshu.chhikara@oimedia.in>
2021-05-04 23:53:15 +0530

    password login and passport authentication

--- routes/web.js
+++ routes/web.js
@@ -8,10 +8,14 @@ function initRoute(app){
     app.get('/', homeController().index)
     
     app.get('/login',authController().login)
+    app.post('/login',authController().postLogin)
     app.get('/register',authController().register)
+    app.post('/register',authController().postRegister)
 
     app.get('/cart',cartController().cart)
     app.post('/update-cart',cartController().update)
+
+
 }
 
 module.exports = initRoute
